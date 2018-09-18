a684ee1b64175d789e9d0dc1c11386c91b38a521
usernamesri <42993131+usernamesri@users.noreply.github.com>
2018-09-18 14:14:29 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,8 +1,8 @@
-def power(base,exp):
+def p(base,exp):
     if(exp==1):
         return(base)
     if(exp!=1):
-        return(base*power(base,exp-1))
+        return(base*p(base,exp-1))
 base=int(input())
 exp=int(input())
-print(power(base,exp))
+print(p(base,exp))
